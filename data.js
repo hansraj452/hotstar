9859d03b7b4d061e028d7e7949bd44aa7d76ada8
Hansraj <phantomprince6386@gmail.com>
2022-09-29 17:08:43 +0530

    clone website

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+      name: 'Falcon and the winter soldier',
+      des:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet sed nisi sed sodales. Pellentesque viverra pulvinar ipsum, sit amet congue dui convallis eget. Pellentesque quis placerat justo. ',
+      image: 'images/slider 1.PNG',
+    },
+    {
+      name: 'Loki',
+      des:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet sed nisi sed sodales. Pellentesque viverra pulvinar ipsum, sit amet congue dui convallis eget. Pellentesque quis placerat justo. ',
+      image: 'images/slider 2.PNG',
+    },
+    {
+      name: 'wanda vision',
+      des:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet sed nisi sed sodales. Pellentesque viverra pulvinar ipsum, sit amet congue dui convallis eget. Pellentesque quis placerat justo. ',
+      image: 'images/slider 3.PNG',
+    },
+    {
+      name: 'raya and the last dragon',
+      des:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet sed nisi sed sodales. Pellentesque viverra pulvinar ipsum, sit amet congue dui convallis eget. Pellentesque quis placerat justo. ',
+      image: 'images/slider 4.PNG',
+    },
+    {
+      name: 'luca',
+      des:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet sed nisi sed sodales. Pellentesque viverra pulvinar ipsum, sit amet congue dui convallis eget. Pellentesque quis placerat justo. ',
+      image: 'images/slider 5.PNG',
+    }
+  ]
